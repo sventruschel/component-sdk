558ef755b00639164a3c51f44fd1ab648618f1d7
kagia <benjamin.kagia@bettyblocks.com>
2021-10-18 13:07:34 +0200

    fix: tsconfig input files

--- tests/prefabs/factories/component.test.ts
+++ tests/prefabs/factories/component.test.ts
@@ -2,10 +2,13 @@ import test from "tape";
 import { component } from "../../../src/prefabs/factories/component";
 
 test("component builds valid component", function (t) {
-  t.deepEqual(component("Text", { options: [] }, []), {
-    name: "Text",
-    options: [],
-    descendants: [],
-  });
+    const result = component("Text", { options: [] }, []);
+    const expected = {
+        name: "Text",
+        options: [],
+        descendants: [],
+      }
+
+  t.deepEqual(result, expected);
   t.end();
 });
