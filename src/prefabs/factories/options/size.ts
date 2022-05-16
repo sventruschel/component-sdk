d9b31054a34f01c6dfb0824cc6b950ac0548a815
michel <michel.dolstra@bettyblocks.com>
2022-05-16 09:44:02 +0200

    feat: new helper for size option

---
+++ src/prefabs/factories/options/size.ts
@@ -0,0 +1,29 @@
+import {
+  PrefabComponentOption,
+  ValueDefault,
+  ValueRef,
+} from '../../types/options';
+
+type OptionProducer = (key: string) => PrefabComponentOption;
+
+// typescript issue #36981
+// Omit is currently desctructive to union/extended types see
+// So we have to Omit each variant as a work around
+type RedundantKeys = 'type' | 'key' | 'label';
+type Attributes =
+  | Omit<ValueDefault, RedundantKeys>
+  | Omit<ValueRef, RedundantKeys>;
+
+const defaultAttributes = {
+  value: [],
+};
+
+export const size =
+  (label: string, attrs: Attributes): OptionProducer =>
+  (key) => ({
+    ...defaultAttributes,
+    ...attrs,
+    key,
+    type: 'SIZE',
+    label,
+  });
